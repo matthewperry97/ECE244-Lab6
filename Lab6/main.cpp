f12f8e4ce231ba019ae46b2e883d2080a435d044
Matthew Perry <perrymatt97@gmail.com>
2016-12-02 16:03:24 -0500

    Initial Commit

---
+++ Lab6/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab6
+//
+//  Created by Matthew Perry on -0212-2016.
+//  Copyright © 2016 Matthew Perry. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
